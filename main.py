3267a73206614e7c51217c20d80037979cb78fdc
Trương Thái Thịnh <truongthaithinh.htn@gmail.com>
2026-06-30 21:42:20 +0700

    BTV N

--- main.py
+++ main.py
@@ -17,4 +17,6 @@ def get_product_detail(product_id: int):
     for product in products:
         if product["id"] == product_id:
             return product
-    return {"message": "Không tìm thấy sản phẩm"}
+    return {
+        "message": "Không tìm thấy sản phẩm"
+    }
